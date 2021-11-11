b9306c861fa2be368720a9326feef5f484a14110
mattndross <mattndross@users.noreply.github.com>
2021-11-11 14:04:11 +0100

    task 1 done

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const express = require("express");
 const app = express();
 
@@ -20,17 +21,21 @@ app.get("/customers", (req, res) => {
 });
 
 app.get("/suppliers", (req, res) => {
-    pool.query("SELECT * from suppliers ", (error, result) => {
-      res.json(result.rows);
-    });
+  pool.query("SELECT * from suppliers ", (error, result) => {
+    res.json(result.rows);
+  });
 });
 
 app.get("/products", (req, res) => {
-    pool.query("select p.product_name , s.supplier_name from products p join suppliers s on p.supplier_id = s.id",
-    (error, result) => {
-        res.json(result.rows);
-    })
-})
+  const productName = req.query.name;
+  let query = "select p.product_name , s.supplier_name from products p join suppliers s on p.supplier_id = s.id";
+  if(productName) {
+    query = query + ` where upper(p.product_name) like upper('%${productName}%') order by p.product_name`;
+  }
+  pool.query(query, (error, result) => { 
+    res.json(result.rows);
+  });
+});
 
 app.listen(3000, () => {
   console.log("server listenin in port 3000");
